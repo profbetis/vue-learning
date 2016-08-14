6553db3cc0f6d9a41d14e823bd6e56b690dc1289
Kevin Weber <mrbetis@gmail.com>
2016-08-14 01:01:59 -0400

    Initial commit

---
+++ src/siteTitle.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="siteTitle">
+        <div id="site-title">{{ siteTitle }}</div>
+        <div id="site-subtitle">{{ siteSubtitle }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      siteTitle: 'Vue Tests',
+      siteSubtitle: 'Planted by Kevin Weber and Bodie Solomon'
+    }
+  }
+}
+</script>
